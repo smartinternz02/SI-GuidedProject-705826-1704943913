11b7bef058d38cc1372afd26628cd56f8bedf0f1
Chetana-Akshaya <chetanaakshayamudragada@gmail.com>
2024-01-26 18:33:40 +0530

    add remaining

--- Scripts/variable_testcase_folder/local_variable_tc_006/Script1706264087025.groovy
+++ Scripts/variable_testcase_folder/local_variable_tc_006/Script1706264087025.groovy
@@ -17,10 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('https://www.amazon.in/')
-
 WebUI.click(findTestObject('Object Repository/Page_Online Shopping site in India Shop Onl_10c5f3/span_Update location'))
 
 WebUI.setText(findTestObject('Object Repository/Page_Online Shopping site in India Shop Onl_10c5f3/inputGLUXZipUpdateInput'), 
